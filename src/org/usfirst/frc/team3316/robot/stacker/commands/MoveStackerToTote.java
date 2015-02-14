b6e2f23ef2ad387d8552bd086e7f717280d49f09
frc3316 <frc3316@gmail.com>
2015-02-14 13:56:57 +0200

    Finished adding PickupSequence and fixed MoveStacker commmands

--- src/org/usfirst/frc/team3316/robot/stacker/commands/MoveStackerToTote.java
+++ src/org/usfirst/frc/team3316/robot/stacker/commands/MoveStackerToTote.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team3316.robot.stacker.commands;
 
 import org.usfirst.frc.team3316.robot.Robot;
+import org.usfirst.frc.team3316.robot.rollerGripper.GamePieceCollected;
 import org.usfirst.frc.team3316.robot.stacker.GamePieceType;
 import org.usfirst.frc.team3316.robot.stacker.StackerPosition;
 
@@ -8,18 +9,12 @@ import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class MoveStackerToTote extends MoveStacker
 {
-	public MoveStackerToTote() 
-    {
-        super();
-    }
-
 	protected void initialize()
 	{
 		/*
 		 * If one of the ratchets is not in place and they should be pressed, dont start
 		 */
-		if (	Robot.stacker.getSolenoidContainer() == DoubleSolenoid.Value.kReverse &&
-				Robot.stacker.getPosition() == StackerPosition.Floor &&
+		if (	Robot.stacker.getPosition() == StackerPosition.Floor &&
 				(!Robot.stacker.getSwitchLeft() || !Robot.stacker.getSwitchRight())) 
 		{
 			this.cancel();
@@ -32,12 +27,17 @@ public class MoveStackerToTote extends MoveStacker
 	
 	protected void setSolenoids() 
 	{
-		if (	Robot.stacker.getStackBase() != null &&
-				Robot.stacker.getStackBase().getType() == GamePieceType.Container)
+		//TODO: check if the condition should be if the command started when the stacker was stuck on a container
+		if (Robot.rollerGripper.getGamePieceCollected() == GamePieceCollected.Container)
 		{
 			Robot.stacker.openSolenoidContainer();
 		}
 		Robot.stacker.closeSolenoidUpper();
     	Robot.stacker.closeSolenoidBottom();
+	}
+	
+	protected boolean isFinished ()
+	{
+		return (Robot.stacker.getPosition() == StackerPosition.Tote); 
 	}
 }
