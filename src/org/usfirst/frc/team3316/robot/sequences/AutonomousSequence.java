cd2fed3a43c20a55989cd392f8e631fc16e0e819
frc3316 <frc3316@gmail.com>
2015-03-05 13:11:38 +0200

    Changed autonomous sequence to movement profile #3

--- src/org/usfirst/frc/team3316/robot/sequences/AutonomousSequence.java
+++ src/org/usfirst/frc/team3316/robot/sequences/AutonomousSequence.java
@@ -11,9 +11,15 @@ public class AutonomousSequence extends CommandGroup
 {    
     public  AutonomousSequence() 
     {
-        addSequential(new RobotOrientedNavigation(1, 0, 0));
-        addSequential(new RobotOrientedNavigation(0, 2, 0));
-        addSequential(new RobotOrientedNavigation(-1, 0, 0));
-        addSequential(new RobotOrientedNavigation(0, 2, 0));
+    	/*
+    	 * Movement profile #3
+    	 */
+        addSequential(new RobotOrientedNavigation(0, 0, 45));
+        addSequential(new RobotOrientedNavigation(-0.707, 0.707, -45)); //1 meter in the original direction
+        addSequential(new RobotOrientedNavigation(0, 1, 0));
+        
+        addSequential(new RobotOrientedNavigation(0, 0, 45));
+        addSequential(new RobotOrientedNavigation(-0.707, 0.707, -45)); //1 meter in the original direction
+        addSequential(new RobotOrientedNavigation(0, 1, 0));
     }
 }
