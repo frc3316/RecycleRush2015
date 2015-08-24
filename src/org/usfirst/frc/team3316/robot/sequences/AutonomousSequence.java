1c665ea3445a786b8263da6dac72484cbe2d5d51
frc3316 <frc3316@gmail.com>
2015-08-24 14:11:37 +0300

    added minor changes to AutonomousSequence.

--- src/org/usfirst/frc/team3316/robot/sequences/AutonomousSequence.java
+++ src/org/usfirst/frc/team3316/robot/sequences/AutonomousSequence.java
@@ -1,5 +1,50 @@
-package org.usfirst.frc.team3316.robot.sequences;
+ package org.usfirst.frc.team3316.robot.sequences;
 
-public class AutonomousSequence {
+import org.usfirst.frc.team3316.robot.Robot;
+import org.usfirst.frc.team3316.robot.chassis.commands.RobotOrientedNavigation;
+import org.usfirst.frc.team3316.robot.logger.DBugLogger;
 
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class AutonomousSequence extends CommandGroup 
+{    
+    public AutonomousSequence() 
+    {
+    	/*
+    	 * Movement profile #3
+    	 */
+    	addSequential(new AutoTotePickup());
+        
+    	addSequential(new SweepContainerSequence());
+    	addParallel(new RobotOrientedNavigation(0, 2.11, 0, 3));
+    	addSequential(new AutoToteCollect());
+    	
+    	addSequential(new SweepContainerSequence());
+    	addParallel(new RobotOrientedNavigation(0, 2.11, 0, 3));
+    	addSequential(new AutoToteCollect());
+    	
+        addSequential(new RobotOrientedNavigation(3.3, 0, -179, 4));
+        
+        addSequential(new DropoffSequence());
+    }
+    
+    DBugLogger logger = Robot.logger;
+    
+    protected void initialize ()
+    {
+    	logger.info(this.getName() + " initialize");
+    }
+    
+    protected void end ()
+    {
+    	logger.info(this.getName() + " end");
+    }
+    
+    protected void interrupted ()
+    {
+    	logger.info(this.getName() + " interrupted");
+    }
 }
