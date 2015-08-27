40a3b1d33509f343262cf69ee4523b6bf2b516d1
Robotica-02 <frc3316@gmail.com>
2015-08-27 12:16:20 +0300

    Debugging

--- src/org/usfirst/frc/team3316/robot/sequences/AutonomousTest.java
+++ src/org/usfirst/frc/team3316/robot/sequences/AutonomousTest.java
@@ -1,42 +1,40 @@
 package org.usfirst.frc.team3316.robot.sequences;
 
+import org.usfirst.frc.team3316.robot.Robot;
 import org.usfirst.frc.team3316.robot.chassis.commands.RobotOrientedNavigation;
+import org.usfirst.frc.team3316.robot.config.Config;
+import org.usfirst.frc.team3316.robot.config.Config.ConfigException;
+import org.usfirst.frc.team3316.robot.logger.DBugLogger;
 import org.usfirst.frc.team3316.robot.rollerGripper.commands.RollContainer;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /**
  *
  */
-public class AutonomousTest extends AutonomousSequence {
+public class AutonomousTest extends CommandGroup {
 
-    public AutonomousTest() {
- 
-    	
+	double pushTime;
+    public AutonomousTest() 
+    {
     	addParallel(new RobotOrientedNavigation(0, 2.11, 0, 3));
     	addSequential(new RollContainer(), pushTime);
     	addSequential(new AutoToteCollect());
     }
+    DBugLogger logger = Robot.logger;
+    Config config= Robot.config;
 
-    // Called just before this Command runs the first time
     protected void initialize() {
-    }
+    	logger.info(this.getName() + " initialize");
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
+    	try
+		{
+    		pushTime = (double) config.get("rollerGripper_PushContainer_PushTime");
+		}
+		catch (ConfigException e)
+		{
+			logger.severe(e);
+		}
     }
 }
