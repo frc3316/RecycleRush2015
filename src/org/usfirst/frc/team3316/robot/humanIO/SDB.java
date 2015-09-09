829b615aee2dbcabc03d9c34174ee87ad42def47
ROBOTICA-02 <frc3316@gmail.com>
2015-09-09 16:55:45 +0300

    Added robot oriented drive navigation buttons.

--- src/org/usfirst/frc/team3316/robot/humanIO/SDB.java
+++ src/org/usfirst/frc/team3316/robot/humanIO/SDB.java
@@ -10,6 +10,7 @@ import java.util.TimerTask;
 
 import org.usfirst.frc.team3316.robot.Robot;
 import org.usfirst.frc.team3316.robot.chassis.commands.FieldOrientedDrive;
+import org.usfirst.frc.team3316.robot.chassis.commands.RobotOrientedNavigation;
 import org.usfirst.frc.team3316.robot.chassis.commands.StrafeDrive;
 import org.usfirst.frc.team3316.robot.chassis.commands.TankDrive;
 import org.usfirst.frc.team3316.robot.chassis.heading.SetHeadingPreset;
@@ -26,19 +27,21 @@ import com.ni.vision.NIVision.Image;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class SDB 
+public class SDB
 {
 	/*
-	 * Runnable that periodically updates values from the robot into the SmartDashboard
-	 * This is the place where all of the robot data should be displayed from
+	 * Runnable that periodically updates values from the robot into the
+	 * SmartDashboard This is the place where all of the robot data should be
+	 * displayed from
 	 */
 	private class UpdateSDBTask extends TimerTask
 	{
-		public UpdateSDBTask ()
+		public UpdateSDBTask()
 		{
 			logger.info("Created UpdateSDBTask");
 		}
-		public void run ()
+
+		public void run()
 		{
 			/*
 			 * Camera
@@ -51,76 +54,78 @@ public class SDB
 			/*
 			 * Robot Info
 			 */
-			put ("Current Heading", Robot.chassis.getHeading());
-			
-			put ("Left Ratchet", Robot.stacker.getSwitchRatchetLeft());
-			put ("Right Ratchet", Robot.stacker.getSwitchRatchetRight());
-			
-			put ("Game Piece Switch", Robot.rollerGripper.getSwitchGamePiece());
+			put("Current Heading", Robot.chassis.getHeading());
+
+			put("Left Ratchet", Robot.stacker.getSwitchRatchetLeft());
+			put("Right Ratchet", Robot.stacker.getSwitchRatchetRight());
+
+			put("Game Piece Switch", Robot.rollerGripper.getSwitchGamePiece());
 		}
-		
-		private void put (String name, double d)
-	    {
-	    	SmartDashboard.putNumber(name, d);
-	    }
-	    
-	    private void put (String name, int i)
-	    {
-	    	SmartDashboard.putInt(name, i);
-	    }
-	    
-	    private void put (String name, boolean b)
-	    {
-	    	SmartDashboard.putBoolean(name, b);
-	    }
-	    
-	    private void put (String name, String s)
-	    {
-	    	SmartDashboard.putString(name, s);
-	    }
+
+		private void put(String name, double d)
+		{
+			SmartDashboard.putNumber(name, d);
+		}
+
+		private void put(String name, int i)
+		{
+			SmartDashboard.putInt(name, i);
+		}
+
+		private void put(String name, boolean b)
+		{
+			SmartDashboard.putBoolean(name, b);
+		}
+
+		private void put(String name, String s)
+		{
+			SmartDashboard.putString(name, s);
+		}
 	}
-	
+
 	DBugLogger logger = Robot.logger;
 	Config config = Robot.config;
-	
+
 	private Image frame;
-	
+
 	private UpdateSDBTask updateSDBTask;
-	
-	private Hashtable <String, Class <?> > variablesInSDB;
-	
-	public SDB ()
+
+	private Hashtable<String, Class<?>> variablesInSDB;
+
+	public SDB()
 	{
-		variablesInSDB = new Hashtable <String, Class <?> > ();
-		
+		variablesInSDB = new Hashtable<String, Class<?>>();
+
 		initSDB();
-		
+
 		if (Robot.sensors.isCameraFound())
 		{
 			frame = NIVision.imaqCreateImage(NIVision.ImageType.IMAGE_RGB, 0);
 		}
 	}
-	
-	public void timerInit ()
+
+	public void timerInit()
 	{
 		updateSDBTask = new UpdateSDBTask();
 		Robot.timer.schedule(updateSDBTask, 0, 100);
 	}
-	
+
 	/**
 	 * Adds a certain key in the config to the SmartDashboard
-	 * @param key the key required
+	 * 
+	 * @param key
+	 *            the key required
 	 * @return whether the value was put in the SmartDashboard
 	 */
-	public boolean putConfigVariableInSDB (String key)
+	public boolean putConfigVariableInSDB(String key)
 	{
 		try
 		{
 			Object value = config.get(key);
-			Class <?> type = value.getClass();
-			
+			Class<?> type = value.getClass();
+
 			boolean constant = Character.isUpperCase(key.codePointAt(0));
-			
+
 			if (type == Double.class)
 			{
 				SmartDashboard.putNumber(key, (double) value);
@@ -133,19 +138,19 @@ public class SDB
 			{
 				SmartDashboard.putBoolean(key, (boolean) value);
 			}
-			
+
 			if (!constant)
 			{
 				variablesInSDB.put(key, type);
-				logger.info("Added to SDB " + key + " of type " + type + 
-						"and allows for its modification");
+				logger.info("Added to SDB " + key + " of type " + type
+						+ "and allows for its modification");
 			}
 			else
 			{
-				logger.info("Added to SDB " + key + " of type " + type + 
-						"BUT DOES NOT ALLOW for its modification");
+				logger.info("Added to SDB " + key + " of type " + type
+						+ "BUT DOES NOT ALLOW for its modification");
 			}
-			
+
 			return true;
 		}
 		catch (ConfigException e)
@@ -154,16 +159,17 @@ public class SDB
 		}
 		return false;
 	}
-	
-	public Set <Entry <String, Class <?> > > getVariablesInSDB ()
+
+	public Set<Entry<String, Class<?>>> getVariablesInSDB()
 	{
 		return variablesInSDB.entrySet();
 	}
-	
-	private void initSDB ()
+
+	private void initSDB()
 	{
-		SmartDashboard.putData(new UpdateVariablesInConfig()); //NEVER REMOVE THIS COMMAND
-		
+		SmartDashboard.putData(new UpdateVariablesInConfig()); // NEVER REMOVE
+																// THIS COMMAND
+
 		/*
 		 * Basic Drives
 		 */
@@ -172,60 +178,93 @@ public class SDB
 		 */
 		SmartDashboard.putData(new TankDrive());
 		SmartDashboard.putData(new StrafeDrive());
-		
+
 		SmartDashboard.putData(new RobotOrientedDrive());
 		SmartDashboard.putData(new FieldOrientedDrive());
-		
+
 		SmartDashboard.putData(new AutonomousTest());
-		
+
+		SmartDashboard.putData("1 meter forward", new RobotOrientedNavigation(
+				0, 1, 0, 3));
+		SmartDashboard.putData("1 meter right", new RobotOrientedNavigation(1,
+				0, 0, 3));
+		SmartDashboard.putData("turn right", new RobotOrientedNavigation(0, 0,
+				90, 1));
 		/*
 		 * Set Heading SDB
 		 */
 		SmartDashboard.putData("Zero Heading", new SetHeadingPreset(0));
-		
+
 		SmartDashboard.putData(new SetHeadingSDB());
 		putConfigVariableInSDB("chassis_HeadingToSet");
 		putConfigVariableInSDB("rollerGripper_RollContainer_LeftSpeed");
 		putConfigVariableInSDB("rollerGripper_RollContainer_RightSpeed");
 		putConfigVariableInSDB("rollerGripper_PushContainer_PushTime");
-		
+
 		/*
 		 * Autonomous test
 		 */
+
+		// set 1
 		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerX_KP_1");
 		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerX_KI_1");
 		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerX_KD_1");
-		
+
 		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerY_KP_1");
 		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerY_KI_1");
 		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerY_KD_1");
-		
+
 		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerHeading_KP_1");
 		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerHeading_KI_1");
 		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerHeading_KD_1");
-		
+
 		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerX_AbsoluteTolerance_1");
 		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerY_AbsoluteTolerance_1");
 		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerHeading_AbsoluteTolerance_1");
-		
+
 		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerX_MinimumOutput_1");
 		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerX_MaximumOutput_1");
-		
+
 		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerY_MinimumOutput_1");
 		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerY_MaximumOutput_1");
-		
+
 		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerHeading_MinimumOutput_1");
 		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerHeading_MaximumOutput_1");
-		
+
 		putConfigVariableInSDB("chassis_RobotOrientedNavigation_MaxFinishCounter_1");
-		
-		
-		
+
+		// set 3
+		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerX_KP_3");
+		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerX_KI_3");
+		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerX_KD_3");
+
+		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerY_KP_3");
+		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerY_KI_3");
+		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerY_KD_3");
+
+		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerHeading_KP_3");
+		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerHeading_KI_3");
+		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerHeading_KD_3");
+
+		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerX_AbsoluteTolerance_3");
+		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerY_AbsoluteTolerance_3");
+		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerHeading_AbsoluteTolerance_3");
+
+		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerX_MinimumOutput_3");
+		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerX_MaximumOutput_3");
+
+		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerY_MinimumOutput_3");
+		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerY_MaximumOutput_3");
+
+		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerHeading_MinimumOutput_3");
+		putConfigVariableInSDB("chassis_RobotOrientedNavigation_PIDControllerHeading_MaximumOutput_3");
+
+		putConfigVariableInSDB("chassis_RobotOrientedNavigation_MaxFinishCounter_3");
+
 		putConfigVariableInSDB("chassis_Velocity_ResetVelocity");
 		putConfigVariableInSDB("chassis_Velocity_Lowpass");
 		putConfigVariableInSDB("chassis_Velocity_UseLowPass");
-		
-		
+
 		logger.info("Finished initSDB()");
 	}
 }
